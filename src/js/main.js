7641a000027445a5a4a392bbac328c1bdfac29f1
Marina Ruiz <marruiart@gmail.com>
2022-11-27 10:39:55 +0100

    add X movement on hamburger menu

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,20 @@
 
 let onoff = true;
 function displayMenu() {
+    let menuIcon = document.getElementById("menu_icon"),
+    childDiv = menuIcon.getElementsByTagName("div")[1];
 
     if (onoff == true) {
         document.getElementById("nav_menu").style.display = "flex";
+        document.getElementById("menu_icon").firstElementChild.style.transform = "rotate(45deg)";
+        childDiv.style.opacity = "0";
+        document.getElementById("menu_icon").lastElementChild.style.transform = "rotate(-45deg)";
         onoff = false;
     } else if (onoff == false) {
         document.getElementById("nav_menu").style.display = "none";
+        document.getElementById("menu_icon").firstElementChild.style.transform = "rotate(0)";
+        childDiv.style.opacity = "1";
+        document.getElementById("menu_icon").lastElementChild.style.transform = "rotate(0)";
         onoff = true;
     }
 }
